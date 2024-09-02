7646ae06e14dd758198190b100fcf976dd9bc8a7
Vladislav Knyazev <knyazev.activ@yandex.ru>
2024-09-02 19:21:21 +0300

    Фиксит название переменной

--- src/view/events-item-view.js
+++ src/view/events-item-view.js
@@ -30,7 +30,7 @@ function createEventsItemTemplate(event, destination, offersByType) {
       `<li class="event__offer">
         <span class="event__offer-title">${offer.title}</span>
         &plus;&euro;&nbsp;
-        <span class="event__offer-price">${offer.basePrice}</span>
+        <span class="event__offer-price">${offer.price}</span>
       </li>`)).join('')}
         </ul>
         <button class="event__favorite-btn ${isFavorite ? 'event__favorite-btn--active' : ''}" type="button">
