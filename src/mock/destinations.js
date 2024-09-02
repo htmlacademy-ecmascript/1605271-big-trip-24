804fb8f950d5bb5b7fa8b5bceb0b85f5a09a3c64
Vladislav Knyazev <knyazev.activ@yandex.ru>
2024-09-02 19:27:20 +0300

    Фиксит содержимое структуры данных пунктов назначения

--- src/mock/destinations.js
+++ src/mock/destinations.js
@@ -9,13 +9,27 @@ const mockDestinations = [
     id: 2,
     name: 'Geneva',
     description: 'Geneva is a city in Switzerland that lies at the southern tip of expansive Lac Léman (Lake Geneva). Surrounded by the Alps and Jura mountains, the city has views of dramatic Mont Blanc.',
-    pictures: [`https://loremflickr.com/248/152?random=${Math.random()}`],
+    pictures: [
+      {
+        src: `https://loremflickr.com/248/152?random=${Math.random()}`,
+        description: 'Тест',
+      },
+    ],
   },
   {
     id: 3,
     name: 'Chamonix',
     description: 'Chamonix-Mont-Blanc (usually shortened to Chamonix) is a resort area near the junction of France, Switzerland and Italy. At the base of Mont Blanc, the highest summit in the Alps, it\'s renowned for its skiing.',
-    pictures: [`https://loremflickr.com/248/152?random=${Math.random()}`, `https://loremflickr.com/248/152?random=${Math.random()}`],
+    pictures: [
+      {
+        src: `https://loremflickr.com/248/152?random=${Math.random()}`,
+        description: 'Тест',
+      },
+      {
+        src: `https://loremflickr.com/248/152?random=${Math.random()}`,
+        description: 'Тест',
+      },
+    ],
   },
 ];
 
